27185275192fbeb17543befdf33864da45dbc36f
Taylor Barnes <taylor.a.barnes@gmail.com>
2022-12-20 14:52:46 -0500

    Update mdimechanic.yml

--- interactive.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo ${SCRIPT_DIR}
-cd ${SCRIPT_DIR}
-
-docker run --rm -v ${SCRIPT_DIR}:/repo -it mdi/qe bash
